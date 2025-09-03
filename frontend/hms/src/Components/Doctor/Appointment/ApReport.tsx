00352a1e146773555a2f5cc42b7d9a4660e14f9d
harsh sahu <harshsahu1143@gmail.com>
2025-09-03 13:34:29 +0530

    Successfully creation of model for medicine show as per the user prescription

--- frontend/hms/src/Components/Doctor/Appointment/ApReport.tsx
+++ frontend/hms/src/Components/Doctor/Appointment/ApReport.tsx
@@ -8,7 +8,7 @@ import {
   Textarea,
   TextInput,
 } from "@mantine/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   dosageFrequencies,
   medicineTypes,
@@ -16,14 +16,23 @@ import {
   symptoms,
   tests,
 } from "../../../Data/DropDownData";
-import { IconTrash } from "@tabler/icons-react";
+import { IconEye, IconSearchOff, IconTrash } from "@tabler/icons-react";
 import { useForm } from "@mantine/form";
-import { createAppointmentReport } from "../../../Service/AppointmentService";
-import { errorNotification, successNotification } from "../../../Utility/NotificationUtil";
+import { createAppointmentReport, getReportByPatientId, isReportExists } from "../../../Service/AppointmentService";
+import {
+  errorNotification,
+  successNotification,
+} from "../../../Utility/NotificationUtil";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { FilterMatchMode } from "primereact/api";
+import { DataTable, DataTableFilterMeta } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { formatDate } from "../../../Utility/DateUtility";
+import { render } from "@testing-library/react";
 
-const ApReport = ({appointment}:any) => {
-  const [loading,setLoading] = React.useState(false);
+const ApReport = ({ appointment }: any) => {
+  const [loading, setLoading] = React.useState(false);
   const dispatch = useDispatch();
   type Medicine = {
     name: string;
@@ -39,31 +48,31 @@ const ApReport = ({appointment}:any) => {
     initialValues: {
       symptoms: [],
       tests: [],
-      diagnosis: '',
-      referral: '',
-      notes: '',
+      diagnosis: "",
+      referral: "",
+      notes: "",
       prescription: {
-   
         medicines: [] as Medicine[],
       },
     },
     validate: {
-      symptoms:(value) => (value.length >0) ? null: 'Please Select at least one ',
-      diagnosis:(value) => (value.length >0) ? null: 'Diagnosis is required',
-      prescription:{
-
-        medicines:{
-          name: value=>(value?.trim() ? null: 'Medicine name is required'),
-          dosage: value=>(value?.trim() ? null: 'Dosage is required'),
-          frequency: value=>(value?.trim() ? null: 'Frequency is required'),
-          route: value=>(value?.trim() ? null: 'Route is required'),
-          type: value=>(value?.trim() ? null: 'Type is required'),
-          instructions: value=>(value?.trim() ? null: 'Instructions are required'),
-        }
-      }
- 
-     
-   }});
+      symptoms: (value) =>
+        value.length > 0 ? null : "Please Select at least one ",
+      diagnosis: (value) => (value.length > 0 ? null : "Diagnosis is required"),
+      prescription: {
+        medicines: {
+          name: (value) => (value?.trim() ? null : "Medicine name is required"),
+          dosage: (value) => (value?.trim() ? null : "Dosage is required"),
+          frequency: (value) =>
+            value?.trim() ? null : "Frequency is required",
+          route: (value) => (value?.trim() ? null : "Route is required"),
+          type: (value) => (value?.trim() ? null : "Type is required"),
+          instructions: (value) =>
+            value?.trim() ? null : "Instructions are required",
+        },
+      },
+    },
+  });
   const insertMedicine = () => {
     form.insertListItem("prescription.medicines", {
       name: "",
@@ -77,167 +86,328 @@ const ApReport = ({appointment}:any) => {
   const removeMedicine = (index: number) => {
     form.removeListItem("prescription.medicines", index);
   };
+
+  useEffect(()=>{
+   fetchData();
+   
+  },[appointment?.patientId, appointment?.id]);
+
+  const fetchData =()=>{
+     if(!appointment?.patientId)return;
+    getReportByPatientId(appointment?.patientId).then((res)=>{
+console.log("Fetched report data:", res);
+setData(res);
+    }).catch((error)=>{
+console.error("Error fetching report data:", error);
+    });
+     isReportExists(appointment?.id).then((res)=>{
+      setAllowAdd(!res);
+      console.log("Report existence check:", res);
+    }).catch((error)=>{
+      console.error("Error checking report existence:", error);
+      setAllowAdd(true);
+    
+    });
+
+  }
   const handleSubmit = (values: typeof form.values) => {
-    console.log("form submitted with value",values);
+    console.log("form submitted with value", values);
     let data = {
       ...values,
-      doctorId:appointment.doctorId,
-      patientId:appointment.patientId,
-      appointmentId:appointment.id,
-      prescription:{
+      doctorId: appointment.doctorId,
+      patientId: appointment.patientId,
+      appointmentId: appointment.id,
+      prescription: {
         ...values.prescription,
-            doctorId:appointment.doctorId,
-      patientId:appointment.patientId,
-      appointmentId:appointment.id,
-      }
-    }
-
+        doctorId: appointment.doctorId,
+        patientId: appointment.patientId,
+        appointmentId: appointment.id,
+      },
+    };
 
     setLoading(true);
     createAppointmentReport(data)
-    .then((res)=>{
-      successNotification("Report created successfully");
-      console.log("Report created successfully:", res);
-      form.reset();
-    }).catch((error) => {
-      errorNotification(error?.response?.data?.errorMessage||"Error creating report");
-      console.error("Error creating report:", error);
-    }).finally(()=>{
-      setLoading(false);
-    })
+      .then((res) => {
+        successNotification("Report created successfully");
+        console.log("Report created successfully:", res);
+        form.reset();
+        setEdit(false);
+        setAllowAdd(false);
+        fetchData();
+      })
+      .catch((error) => {
+        errorNotification(
+          error?.response?.data?.errorMessage || "Error creating report"
+        );
+        console.error("Error creating report:", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
+  const [filters, setFilters] = useState<DataTableFilterMeta>({
+    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  });
+  const onGlobalFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    let _filters: any = { ...filters };
+
+    _filters["global"].value = value;
+
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+  };
+  const [data,setData] = useState<any[]>([]);
+  const[allowAdd,setAllowAdd] = useState<boolean>(false);
+  const [edit,setEdit] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const [globalFilterValue, setGlobalFilterValue] = useState<string>("");
+  const renderHeader = () => {
+    return (
+      <div className="flex flex-wrap gap-2 justify-between tems-center">
+        {allowAdd && <Button onClick={()=>setEdit(true)} variant="filled">Add Report</Button>}
+        <TextInput
+          leftSection={<IconSearchOff />}
+          fw={400}
+          value={globalFilterValue}
+          onChange={onGlobalFilterChange}
+          placeholder="Keyword Search"
+        />
+      </div>
+    );
+  };
+  const header = renderHeader();
+  const actionBodyTemplate = (rowData: any) => {
+    return (
+      <div className="flex gap-2">
+        <ActionIcon
+          onClick={() =>
+            navigate("/doctor/appointment/" + rowData.appointmentId)
+          }
+        >
+          <IconEye size={20} stroke={1.5} />
+        </ActionIcon>
+        
+      </div>
+    );
+  };
+
   return (
-    <form onSubmit={form.onSubmit(handleSubmit)}>
-      <Fieldset
-        className="grid gap-5 grid-cols-2"
-        legend={
-          <span className="text-lg font-medium text-primary-500">
-            Personal information
-          </span>
-        }
-        radius="md"
+    <div>
+     { !edit? <DataTable
+        header={header}
+        stripedRows
+        size="small"
+        value={data}
+        paginator
+        rows={10}
+        paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+        rowsPerPageOptions={[10, 25, 50]}
+        dataKey="id"
+        filterDisplay="menu"
+        globalFilterFields={["patientName", "notes"]}
+        emptyMessage="No Appointment found."
+        currentPageReportTemplate="Showing {first} to {last} of {totalRecords} entries"
       >
-        <MultiSelect
-          {...form.getInputProps("symptoms")}
-          className="col-span-2"
-          label="Symptoms"
-          placeholder="Pick symtoms"
-          data={symptoms}
-        />
-        <MultiSelect
-          {...form.getInputProps("tests")}
-          className="col-span-2"
-          withAsterisk
-          label="Tests"
-          placeholder="Pick tests"
-          data={tests}
-        />
-        <TextInput
-          {...form.getInputProps("diagnosis")}
-          label="Diagnosis"
-          placeholder="Enter Diagnosis"
-          withAsterisk
-        />
-        <TextInput
-          {...form.getInputProps("referral")}
-          label="Referral"
-          placeholder="Enter Referral details"
-        />
-        <Textarea
-          {...form.getInputProps("notes")}
-          className="col-span-2"
-          label="Notes"
-          placeholder="Enter any additional notes"
-      
-        />
-      </Fieldset>
-      <Fieldset
-        className="grid gap-5 "
-        legend={
-          <span className="text-lg font-medium text-primary-500">
-            Prescription
-          </span>
-        }
-        radius="md"
-      >
-   
-        {
-          form.values.prescription.medicines.map((_medicine:Medicine,index:number)=>        
+        <Column field="doctorName" header="Doctor" />
 
+        <Column
+          field="diagnosis"
+          header="Diagnosis"
+          body={(rowData) => rowData.diagnosis ?? ""}
+        />
+        <Column
+          field="reportDate"
+          header="Report Date"
+          sortable
+          style={{ minWidth: "14rem" }}
+          body={(rowData) => <>{formatDate(rowData.createdAt)}</>}
+        />
 
-        (<Fieldset  legend={    <div className=" flex gap-5 items-center">
-            <h1 className="text-lg font-medium">Medicine {index + 1}</h1>
-            <ActionIcon onClick={()=> removeMedicine(index)} variant="filled" color="red" size="md" className="mb-2">
-              <IconTrash />
-            </ActionIcon>
+  
+
+        <Column field="notes" header="Notes"  />
+        {/* <Column
+          headerStyle={{ width: "5rem", textAlign: "center" }}
+          bodyStyle={{ textAlign: "center", overflow: "visible" }}
+          body={actionBodyTemplate}
+        /> */}
+        {/* <Column
+                     headerStyle={{ width: "5rem", textAlign: "center" }}
+                     bodyStyle={{ textAlign: "center", overflow: "visible" }}
+                     body={actionBodyTemplate}
+                   /> */}
+      </DataTable>
+
+      :<form onSubmit={form.onSubmit(handleSubmit)}>
+        <Fieldset
+          className="grid gap-5 grid-cols-2"
+          legend={
+            <span className="text-lg font-medium text-primary-500">
+              Personal information
+            </span>
+          }
+          radius="md"
+        >
+          <MultiSelect
+            {...form.getInputProps("symptoms")}
+            className="col-span-2"
+            label="Symptoms"
+            placeholder="Pick symtoms"
+            data={symptoms}
+          />
+          <MultiSelect
+            {...form.getInputProps("tests")}
+            className="col-span-2"
+            withAsterisk
+            label="Tests"
+            placeholder="Pick tests"
+            data={tests}
+          />
+          <TextInput
+            {...form.getInputProps("diagnosis")}
+            label="Diagnosis"
+            placeholder="Enter Diagnosis"
+            withAsterisk
+          />
+          <TextInput
+            {...form.getInputProps("referral")}
+            label="Referral"
+            placeholder="Enter Referral details"
+          />
+          <Textarea
+            {...form.getInputProps("notes")}
+            className="col-span-2"
+            label="Notes"
+            placeholder="Enter any additional notes"
+          />
+        </Fieldset>
+        <Fieldset
+          className="grid gap-5 "
+          legend={
+            <span className="text-lg font-medium text-primary-500">
+              Prescription
+            </span>
+          }
+          radius="md"
+        >
+          {form.values.prescription.medicines.map(
+            (_medicine: Medicine, index: number) => (
+              <Fieldset
+                legend={
+                  <div className=" flex gap-5 items-center">
+                    <h1 className="text-lg font-medium">
+                      Medicine {index + 1}
+                    </h1>
+                    <ActionIcon
+                      onClick={() => removeMedicine(index)}
+                      variant="filled"
+                      color="red"
+                      size="md"
+                      className="mb-2"
+                    >
+                      <IconTrash />
+                    </ActionIcon>
+                  </div>
+                }
+                className="grid gap-5 col-span-2 grid-cols-2"
+              >
+                <TextInput
+                  {...form.getInputProps(
+                    `prescription.medicines.${index}.name`
+                  )}
+                  label="Medicine"
+                  placeholder="Enter medicineName"
+                  withAsterisk
+                />
+                <TextInput
+                  {...form.getInputProps(
+                    `prescription.medicines.${index}.dosage`
+                  )}
+                  label="Dosage"
+                  placeholder="Enter dosage "
+                  withAsterisk
+                />
+
+                <Select
+                  {...form.getInputProps(
+                    `prescription.medicines.${index}.frequency`
+                  )}
+                  label="Frequency"
+                  placeholder="Pick frequency"
+                  data={dosageFrequencies}
+                  withAsterisk
+                />
+                <NumberInput
+                  {...form.getInputProps(
+                    `prescription.medicines.${index}.duration`
+                  )}
+                  label="Duration (in days)"
+                  placeholder="Enter duration"
+                  withAsterisk
+                />
+
+                <Select
+                  label="Route"
+                  {...form.getInputProps(
+                    `prescription.medicines.${index}.route`
+                  )}
+                  placeholder="Pick route"
+                  data={routess}
+                  withAsterisk
+                />
+                <Select
+                  label="Type"
+                  {...form.getInputProps(
+                    `prescription.medicines.${index}.type`
+                  )}
+                  placeholder="Select type"
+                  data={medicineTypes}
+                  withAsterisk
+                />
+                <TextInput
+                  label="Instructions"
+                  {...form.getInputProps(
+                    `prescription.medicines.${index}.instructions`
+                  )}
+                  placeholder="Enter instructions"
+                  withAsterisk
+                />
+              </Fieldset>
+            )
+          )}
+          <div className="flex items-center justify-center col-span-2">
+            <Button
+              onClick={insertMedicine}
+              variant="outline"
+              color="blue"
+              className=" col-span-2 mt-4"
+            >
+              Add Medicine
+            </Button>
           </div>
-        } className="grid gap-5 col-span-2 grid-cols-2">
-      
-
-          <TextInput
-          {...form.getInputProps(`prescription.medicines.${index}.name`)}
-            label="Medicine"
-            placeholder="Enter medicineName"
-            withAsterisk
-          />
-          <TextInput
-          {...form.getInputProps(`prescription.medicines.${index}.dosage`)}
-            label="Dosage"
-            placeholder="Enter dosage "
-            withAsterisk
-          />
-
-          <Select
-            {...form.getInputProps(`prescription.medicines.${index}.frequency`)}
-            label="Frequency"
-            placeholder="Pick frequency"
-            data={dosageFrequencies}
-            withAsterisk
-          />
-          <NumberInput
-            {...form.getInputProps(`prescription.medicines.${index}.duration`)}
-            label="Duration (in days)"
-            placeholder="Enter duration"
-            withAsterisk
-          />
-
-          <Select 
-
-            label="Route"
-            {...form.getInputProps(`prescription.medicines.${index}.route`)}
-            placeholder="Pick route"
-            data={routess}
-            withAsterisk
-          />
-          <Select
-            label="Type"
-            {...form.getInputProps(`prescription.medicines.${index}.type`)}
-            placeholder="Select type"
-            data={medicineTypes}
-            withAsterisk
-          />
-          <TextInput
-            label="Instructions"
-            {...form.getInputProps(`prescription.medicines.${index}.instructions`)}
-            placeholder="Enter instructions"
-            withAsterisk
-          />
-        </Fieldset>))}
-      <div className="flex items-center justify-center col-span-2">
-            <Button onClick={insertMedicine} variant="outline" color="blue" className=" col-span-2 mt-4">
-          Add Medicine
-        </Button>
-      </div>
-      </Fieldset>
-      <div className="flex items-center gap-5 justify-center mt-4">
-        <Button loading={loading} type="submit" className="w-full" variant="filled" color="primary">
-          Submit Report
-        </Button>
-        <Button  loading={loading} className="w-full" variant="filled" color="red">
-          Cancel
-        </Button>
-      </div>
-    </form>
+        </Fieldset>
+        <div className="flex items-center gap-5 justify-center mt-4">
+          <Button
+            loading={loading}
+            type="submit"
+            className="w-full"
+            variant="filled"
+            color="primary"
+          >
+            Submit Report
+          </Button>
+          <Button
+            loading={loading}
+            className="w-full"
+            variant="filled"
+            color="red"
+          >
+            Cancel
+          </Button>
+        </div>
+      </form>
+}</div>
   );
 };
 
--- frontend/hms/src/Components/Doctor/Appointment/Prescription.tsx
+++ frontend/hms/src/Components/Doctor/Appointment/Prescription.tsx
@@ -1,5 +1,5 @@
-import { ActionIcon, Button, TextInput } from "@mantine/core";
-import { IconEye, IconSearchOff, IconTrash } from "@tabler/icons-react";
+import { ActionIcon, Button, Card, Divider, Grid, Modal, Text, TextInput, Title } from "@mantine/core";
+import { IconEye, IconMedicineSyrup, IconSearchOff, IconTrash } from "@tabler/icons-react";
 import { FilterMatchMode, FilterOperator } from "primereact/api";
 import { Column } from "primereact/column";
 import { DataTable, DataTableFilterMeta } from "primereact/datatable";
@@ -8,8 +8,10 @@ import { getPrescriptionByPatientId } from "../../../Service/AppointmentService"
 import { formatDate } from "../../../Utility/DateUtility";
 import { Tag } from "primereact/tag";
 import { useNavigate } from "react-router-dom";
+import { useDisclosure } from "@mantine/hooks";
 
 const Prescription = ({ appointment }: any) => {
+    const [opened,{open,close}] = useDisclosure(false);
   const [data, setData] = useState<any[]>([]);
   const [filters, setFilters] = useState<DataTableFilterMeta>({
     global: { value: null, matchMode: FilterMatchMode.CONTAINS },
@@ -19,10 +21,13 @@ const Prescription = ({ appointment }: any) => {
         <ActionIcon onClick={()=> navigate("/doctor/appointment/"+rowData.appointmentId)}>
             <IconEye size={20} stroke={1.5} />
         </ActionIcon>
+        <ActionIcon onClick={()=>handleMedicine(rowData.medicines)}>
+            <IconMedicineSyrup size={20} stroke={1.5} />
+        </ActionIcon>
         
     </div>
   };
-
+   const [medicineData,setMedicineData] = useState<any[]>([]);
   const onGlobalFilterChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     let _filters: any = { ...filters };
@@ -37,7 +42,7 @@ const Prescription = ({ appointment }: any) => {
   const [globalFilterValue, setGlobalFilterValue] = useState<string>("");
    console.log("patientId:", appointment?.patientId);
   useEffect(() => {
-
+if(!appointment?.patientId)return;
 
     getPrescriptionByPatientId(appointment?.patientId)
     
@@ -49,6 +54,12 @@ const Prescription = ({ appointment }: any) => {
         console.error("Error fetching Prescription data:", err);
       });
   },[appointment?.patientId]);
+
+  const handleMedicine = (medicines:any[])=>{
+
+    open();
+    setMedicineData(medicines);
+  }
   const renderHeader = () => {
     return (
       <div className="flex flex-wrap gap-2 justify-end tems-center">
@@ -111,6 +122,69 @@ const Prescription = ({ appointment }: any) => {
                body={actionBodyTemplate}
              /> */}
       </DataTable>
+       <Modal opened={opened} size="xl" onClose={close} title="Medicines" centered>
+<div className="grid grid-cols-2 gap-5">
+
+        {
+          medicineData?.map((data:any,index:number)=>(
+
+          
+        <Card key={index} shadow="md" radius="md" withBorder padding="lg">
+
+          <Title order={4} mb="sm" >
+
+            {data.name} {data.type}
+
+          </Title>
+          <Divider my="sm" />
+          <Grid>
+           <Grid.Col span={6}>
+            <Text size="sm" fw={500} >Dosage:</Text>
+            <Text>{data.dosage}</Text>
+           </Grid.Col>
+           <Grid.Col span={6}>
+            <Text size="sm" fw={500} >Frequency:</Text>
+            <Text>{data.frequency}</Text>
+           </Grid.Col>
+           <Grid.Col span={6}>
+            <Text size="sm" fw={500} >Duration:</Text>
+            <Text>{data.duration}</Text>
+           </Grid.Col>
+           <Grid.Col span={6}>
+            <Text size="sm" fw={500} >Route:</Text>
+            <Text>{data.route}</Text>
+           </Grid.Col>
+           <Grid.Col span={6}>
+            <Text size="sm" fw={500} >Prescription ID:</Text>
+            <Text>{data.prescriptionId}</Text>
+           </Grid.Col>
+           <Grid.Col span={6}>
+            <Text size="sm" fw={500} >Medicine ID:</Text>
+            <Text>{data.medicineId}</Text>
+           </Grid.Col>
+           <Grid.Col span={6}>
+            <Text size="sm" fw={500} >Instructions:</Text>
+            <Text>{data.instructions}</Text>
+           </Grid.Col>
+          </Grid>
+
+
+
+
+
+        </Card>
+          ))}
+  
+</div>
+          {
+            medicineData.length ==0&&(
+              <Text color="dimmed" size="sm" mt="md" >
+                No medicines Prescribed for this appointment.
+              </Text>
+            )
+          }
+      </Modal>
+
     </div>
   );
 };
