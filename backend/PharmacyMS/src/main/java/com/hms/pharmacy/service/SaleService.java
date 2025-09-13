c56d44eae44f607d635f54465ec3fe3c53329d64
harsh sahu <harshsahu1143@gmail.com>
2025-09-14 01:19:36 +0530

    Billing & sales service are created!!

---
+++ backend/PharmacyMS/src/main/java/com/hms/pharmacy/service/SaleService.java
@@ -0,0 +1,14 @@
+package com.hms.pharmacy.service;
+
+import com.hms.pharmacy.dto.SaleDTO;
+import com.hms.pharmacy.entity.Sale;
+import com.hms.pharmacy.exception.HmsException;
+
+public interface SaleService {
+
+    Long createSale(SaleDTO dto) throws HmsException;
+    void updateSale(SaleDTO dto) throws HmsException;
+     SaleDTO getSale(Long id) throws HmsException;
+     SaleDTO getSaleByPrescriptionId(Long prescriptionId) throws HmsException;
+
+}
